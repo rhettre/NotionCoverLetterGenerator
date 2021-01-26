11b789739c3871bbff892d5b41ea1ef93eb3dc1f
Rhett Reisman <rhettre@Rhetts-MacBook-Pro.local>
2021-01-25 19:12:01 -0600

    Established connection to Notion + navigated through schema

---
+++ notionTest.py
@@ -0,0 +1,39 @@
+from notion.client import NotionClient
+
+my_token = "3ca4924184f463dc694abc3700f58f28028c42ffd83d9d5be480121eb4c1ad864f1c3604257b1879b1d88d455f09ae93197137d9268801d75bf5508f947a53b2b92bdbe745f249eb5d9d2a7dd895"
+client = NotionClient(token_v2 = my_token)
+
+#lookup ids for different columns
+cover_letter_id = 'A_u<'
+position_id = 'xyVl'
+status_id = 'RpsQ'
+location_id = 'SXHt'
+resume_id = 'XW]^'
+where_did_you_find_job_id = 'Hv_n'
+why_this_job_id = 'WB`~'
+
+cv = client.get_collection_view("https://www.notion.so/rhettre/a1808aae40504e88ba013ccf46069a1a?v=bb1cc2c7f7d64d3298ed4068c7df1ca5")
+
+rows = cv.collection.get_rows()
+
+first_row = rows[0]
+print(first_row.get_property(cover_letter_id))
+print(first_row.get_property(position_id))
+print(first_row.get_property(status_id))
+print(first_row.get_property(location_id))
+print(first_row.get_property(resume_id))
+print(first_row.get_property(where_did_you_find_job_id))
+print(first_row.get_property(why_this_job_id))
+
+
+for row in rows:
+    #Collect all where the the cover letter ID is false 
+    if not row.get_property(cover_letter_id):
+        print(f"Cover letter id for {row.title} is false") 
+
+        #Start Parsing the Data from the Row into your cover letter verbiage
+        #Export to Microsoft Word file using python-docx 
+        #Save as PDF
+
+        #Set the cover letter ID to true
+
